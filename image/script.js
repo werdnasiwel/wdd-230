fef0ab4abe5f4a3d3eae6e7270d3552eed32bcdd
Justin S Murrey <mur19033@byui.edu>
2021-09-13 10:12:31 -0400

    6th commit

--- image/script.js
+++
@@ -1,9 +0,0 @@
-/*todays date and time*/
-const options = {
-    weekday: 'long',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-};
-
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options)
