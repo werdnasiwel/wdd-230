b1e5c9ab1482d2d7a969d4211db73f6c846609f3
Justin S Murrey <mur19033@byui.edu>
2021-09-17 17:27:09 -0400

    date and restart

--- js/script.js
+++ js/script.js
@@ -3,8 +3,7 @@ const options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
-    year: 'numeric',
-
+    year: 'numeric'
 };
-const
-    document.getElementById ('currentdate').textContent = new Date().toLocaleDateString('en-US', options)
+
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
